18783cce3005ae0750e96812736233d45922c727
Tifany <tifsnunes@gmail.com>
2024-04-21 16:57:05 -0300

    page terms

---
+++ app/routes/_layout.termos.tsx
@@ -0,0 +1,52 @@
+export default function Termos() {
+  return (
+    <div>
+      <main>
+        <div className="titulo-bg">
+          <div className="titulo container">
+            <p className="font-2-l-b cor-5">Termos de uso</p>
+            <h1 className="font-1-xxl cor-0">
+              termos e condições<span className="cor-p1">.</span>
+            </h1>
+          </div>
+        </div>
+
+        <div className="termos font-2-s cor-10 container">
+          <h2 className="font-1-l cor-11">1. Política de Estorno</h2>
+          <p>
+            1.1 Aceitamos pagamentos parcelados em todos os cartões de crédito.
+            Para pagamentos à vista também aceitamos PIX e Boleto. Aceitamos
+            pagamentos parcelados em todos os cartões de crédito. Para
+            pagamentos à vista também aceitamos PIX e Boleto.
+          </p>
+          <p>
+            1.2 Aceitamos pagamentos parcelados em todos os cartões de crédito.
+            Para pagamentos à vista também aceitamos PIX e Boleto. Aceitamos
+            pagamentos parcelados em todos os cartões de crédito. Para
+            pagamentos à vista também aceitamos PIX e Boleto.
+          </p>
+          <p>
+            1.3 Aceitamos pagamentos parcelados em todos os cartões de crédito.
+            Para pagamentos à vista também aceitamos PIX e Boleto. Aceitamos
+            pagamentos parcelados em todos os cartões de crédito. Para
+            pagamentos à vista também aceitamos PIX e Boleto.
+          </p>
+          <h2 className="font-1-l cor-11">2. Conta do Usuário</h2>
+          <p>
+            2.1 Aceitamos pagamentos parcelados em todos os cartões de crédito.
+            Para pagamentos à vista também aceitamos PIX e Boleto. Aceitamos
+            pagamentos parcelados em todos os cartões de crédito. Para
+            pagamentos à vista também aceitamos PIX e Boleto.
+          </p>
+          <h2 className="font-1-l cor-11">3. Seguro Bikcraft</h2>
+          <p>
+            3.1 Aceitamos pagamentos parcelados em todos os cartões de crédito.
+            Para pagamentos à vista também aceitamos PIX e Boleto. Aceitamos
+            pagamentos parcelados em todos os cartões de crédito. Para
+            pagamentos à vista também aceitamos PIX e Boleto.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
